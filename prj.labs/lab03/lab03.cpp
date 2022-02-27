14da12138e27cec89cd60507c81a5cc250373396
Grigorii <pangr33n@gmail.com>
2022-02-27 17:28:16 +0300

    lab03

--- prj.labs/lab03/lab03.cpp
+++ prj.labs/lab03/lab03.cpp
@@ -1,6 +1,49 @@
-#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <cmath>
 
 int main() {
-    std::cout << "Hello";
+    cv::Mat img = cv::imread("../data/cross_0256x0256.png");
+    cv::Mat imggray = cv::imread("../data/cross_0256x0256.png", cv::IMREAD_GRAYSCALE);
+
+    cv::imwrite("lab03_rgb.png", img);
+    cv::imwrite("lab03_gre.png", imggray);
+
+    cv::Mat func(512, 512, CV_8UC1);
+    func = 255;
+
+    cv::Mat lut(1, 256, CV_8U);
+    
+
+    //Функция преобразования яркости
+    for(int i = 0; i < 256; i++) {
+        lut.at<uchar>(i) = 256 * std::log(i/256. + 1);
+    }
+
+    //Визуализация функции
+    for(int i = 0; i < 512; i++) {
+        cv::line(
+            func, 
+            cv::Point((i - 1) * 2, 512 - lut.at<uchar>(i - 1) * 4), 
+            cv::Point(i * 2, 512 - lut.at<uchar>(i) * 4),
+            0, 1, 0
+        );
+    }
+    
+    cv::imshow("func", func);
+    cv::imwrite("lab03_viz_func.png", func);
+
+    //LUT
+    cv::Mat imgres;
+    cv::Mat imggrayres;
+    cv::LUT(img, lut, imgres);
+    cv::LUT(imggray, lut, imggrayres);
+
+    cv::imshow("imgres", imgres);
+    cv::imshow("imggrayres", imggrayres);
+    cv::imwrite("lab03_gre_res.png", imggrayres);
+    cv::imwrite("lab03_rgb_res.png", imgres);
+    
+    
+    cv::waitKey(0);
     return 0;
 }
